a5ef49a820896af771e4546ad247c7246eab691c
Janick <pratiksuthar2624@gmail.com>
2024-06-14 17:23:34 +0530

    first commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable react/prop-types */
+const Button = ({ children, onClick }) => {
+	return (
+		<button className="button" onClick={onClick}>
+			{children}
+		</button>
+	);
+};
+export default Button;
---
+++ src/components/FriendsList.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/prop-types */
+import Friend from "./Friend";
+
+const FriendsList = ({ friends, onSelection, selectedFriend }) => {
+	return (
+		<ul>
+			{friends.map((friend) => (
+				<Friend
+					friend={friend}
+					key={friend.id}
+					selectedFriend={selectedFriend}
+					onSelection={onSelection}
+				/>
+			))}
+		</ul>
+	);
+};
+export default FriendsList;
